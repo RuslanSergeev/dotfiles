df9f2047c275a9c795bc5c385c0bfa23309e5427
Ruslan Sergeev <mybox.sergeev@ya.ru>
2021-12-14 11:49:54 +0200

    temporarilly moved back to coc.nvim

--- config/nvim/lsp.lua
+++ config/nvim/lsp.lua
@@ -15,12 +15,12 @@ lspconfig.ccls.setup {
 
 require'lspconfig'.ccls.setup{}
 
-local lsp_installer = require("nvim-lsp-installer")
+-- local lsp_installer = require("nvim-lsp-installer")
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
+-- lsp_installer.on_server_ready(function(server)
+--    local opts = {}
 
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
@@ -29,6 +29,6 @@ lsp_installer.on_server_ready(function(server)
 
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-end)
+--     server:setup(opts)
+-- end)
 
